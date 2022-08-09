47b2c6fc932c93efd37f7cbe5ac513576b56e3e0
Thayane Quintanilha <thayaniquintanilha2@gmail.com>
2022-08-09 15:30:01 -0300

    Requisito 1 feito, mas com erros de chamada no lint

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,32 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testar se fetchProducts é uma função!', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+
+  it('Testar se ao executar a função fetchProducts com o argumento "computador" e testar se ela foi chamada! ', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  });
+
+  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    const result = await fetchProducts('computador');
+    expect(result).toEqual(computadorSearch);
+  });
+
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url"', async () => {
+    try {
+      expect.assertions(1);
+      await fetchProducts();
+    } catch(error) {
+      expect(error).toEqual("You must provide an url");
+    };
+  });
+
 });
